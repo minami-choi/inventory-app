7ea8b780a890ee43fc592741327dd8736b3c44a9
minami-choi <cmina2846@gmail.com>
2018-06-12 01:08:14 +0900

    semantic style 적용

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Product } from '../product.model';
 // ProductRows 전체를 렌더링하고 현재 선택된 Product를 저장하는 컴포넌트
 
 @Component({
-  selector: 'product-list',
+  selector: 'products-list',
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent implements OnInit {
@@ -12,7 +12,7 @@ export class ProductListComponent implements OnInit {
   // 전달된 product[]
   // input의 매개변수는 외부 컴포넌트에서 app-product-list의 input바인딩할 변수로 쓰임
   // 만약 생략된다면 이 컴포넌트에서 쓰일 productList와 같을 경우..
-  @Input('productList-out') productList: Product[];
+  @Input() productList: Product[];
   // 새 product가 선택될 때마다 현재 product를 출력 
   @Output() onProductSelected: EventEmitter<Product>;
 
